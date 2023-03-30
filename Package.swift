f4677e31813872da18a22b55f2c7a4d05735437e
Aaron Taylor <aaron@exphat.com>
2023-03-30 15:21:13 -0400

    Declare test resources

--- Package.swift
+++ Package.swift
@@ -10,7 +10,7 @@ let package = Package(
     targets: [
         .target(name: "Whisper", dependencies: [.target(name: "whisper_cpp")]),
         .target(name: "whisper_cpp", cSettings: [.unsafeFlags(["-O3","-DGGML_USE_ACCELERATE=1"])]),
-        .testTarget(name: "WhisperTests", dependencies: [.target(name: "Whisper")])
+        .testTarget(name: "WhisperTests", dependencies: [.target(name: "Whisper")], resources: [.copy("TestResources/")])
     ],
     cxxLanguageStandard: CXXLanguageStandard.cxx11
 )
