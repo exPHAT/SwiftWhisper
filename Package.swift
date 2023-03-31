91cab8616d9ea83caef1fb985df40b0216c4d148
Aaron Taylor <aaron@exphat.com>
2023-03-31 13:45:26 -0400

    Remove SwiftLink as plugin

--- Package.swift
+++ Package.swift
@@ -1,21 +1,13 @@
-// swift-tools-version:5.7
+// swift-tools-version:5.6
 import PackageDescription
 
 let package = Package(
     name: "Whisper",
-    platforms: [.macOS(.v12), .iOS(.v13)],
     products: [
-        .library(name: "Whisper",
-                 targets: ["Whisper"])
-    ],
-    dependencies: [
-        .package(url: "https://github.com/realm/SwiftLint.git", from: "0.51.0")
+        .library(name: "Whisper", targets: ["Whisper"])
     ],
     targets: [
-        .target(name: "Whisper",
-                dependencies: [.target(name: "whisper_cpp")],
-                plugins: [.plugin(name: "SwiftLintPlugin", package: "SwiftLint")]),
-
+        .target(name: "Whisper", dependencies: [.target(name: "whisper_cpp")]),
         .target(name: "whisper_cpp",
                 cSettings: [.unsafeFlags(["-O3","-DGGML_USE_ACCELERATE=1", "-Wno-everything"])]),
         .testTarget(name: "WhisperTests", dependencies: [.target(name: "Whisper")], resources: [.copy("TestResources/")])
