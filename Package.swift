b1ba443c5aab2a80df6d1d6167f575295e54f30d
Aaron Taylor <aaron@exphat.com>
2023-08-16 22:55:37 -0400

    Exclude CoreML source files from linux build

--- Package.swift
+++ Package.swift
@@ -1,6 +1,13 @@
 // swift-tools-version:5.5
 import PackageDescription
 
+var exclude: [String] = []
+
+#if os(Linux)
+// Linux doesn't support CoreML, and will attempt to import the coreml source directory
+exclude.append("coreml")
+#endif
+
 let package = Package(
     name: "SwiftWhisper",
     products: [
@@ -8,12 +15,15 @@ let package = Package(
     ],
     targets: [
         .target(name: "SwiftWhisper", dependencies: [.target(name: "whisper_cpp")]),
-        .target(name: "whisper_cpp", cSettings: [
-            .define("GGML_USE_ACCELERATE", .when(platforms: [.macOS, .macCatalyst, .iOS])),
-            .define("WHISPER_USE_COREML", .when(platforms: [.macOS, .macCatalyst, .iOS])),
-            .define("WHISPER_COREML_ALLOW_FALLBACK", .when(platforms: [.macOS, .macCatalyst, .iOS]))
-        ]),
+        .target(name: "whisper_cpp",
+                exclude: exclude,
+                cSettings: [
+                    .define("GGML_USE_ACCELERATE", .when(platforms: [.macOS, .macCatalyst, .iOS])),
+                    .define("WHISPER_USE_COREML", .when(platforms: [.macOS, .macCatalyst, .iOS])),
+                    .define("WHISPER_COREML_ALLOW_FALLBACK", .when(platforms: [.macOS, .macCatalyst, .iOS]))
+                ]),
         .testTarget(name: "WhisperTests", dependencies: [.target(name: "SwiftWhisper")], resources: [.copy("TestResources/")])
     ],
     cxxLanguageStandard: CXXLanguageStandard.cxx11
 )
+
