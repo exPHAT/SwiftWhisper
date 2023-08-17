939e533c5f49767a725d2afde4684ce6227b9109
Aaron Taylor <aaron@exphat.com>
2023-08-16 22:56:00 -0400

    Add @available entries for watchOS

--- Tests/WhisperTests/Model Tests/LoadModelTests.swift
+++ Tests/WhisperTests/Model Tests/LoadModelTests.swift
@@ -3,13 +3,13 @@ import XCTest
 @testable import SwiftWhisper
 
 class LoadModelTests: ResourceDependentTestCase, ModelFileTestCase {
-    @available(iOS 13, macOS 10.15, *)
+    @available(iOS 13, macOS 10.15, watchOS 6.0, *)
     func testLoadModelFromFile() async {
         let modelURL = await tinyModelURL!
         let _ = Whisper(fromFileURL: modelURL)
     }
 
-    @available(iOS 13, macOS 10.15, *)
+    @available(iOS 13, macOS 10.15, watchOS 6.0, *)
     func testLoadModelFromData() async throws {
         let modelURL = await tinyModelURL!
         let modelData = try Data(contentsOf: modelURL)
